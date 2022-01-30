6803d5cdb4cc01d71a176f6c82c0a146d63f2064
USM-Cat201 <98682288+USM-Cat201@users.noreply.github.com>
2022-01-30 15:16:54 +0800

    Update AddNewCustomer.java

--- src/electricity/billing/system/AddNewCustomer.java
+++ src/electricity/billing/system/AddNewCustomer.java
@@ -1,41 +1,44 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+/*class for add new customer function in admin homepage */
 package electricity.billing.system;
 
-import java.awt.*;
-import java.awt.event.*;
 import javax.swing.*;
 import java.sql.*;
 import java.util.*;
+import java.awt.*;
+import java.awt.event.*;
+
 
 public class AddNewCustomer extends JFrame implements ActionListener{
-    JLabel JL1,JL2,JL3,JL4,JL5,JL6,JL7,JL8, JL11;
-    JTextField JT1,JT2,JT3,JT4,JT5,JT6,JT7;
-    JButton JB1,JB2;
+    JLabel JL1,JL2,JL3,JL4,JL5,JL6,JL7,JL8, JL11;//declare label JL1 to JL8 for eight different statement
+    JTextField JT1,JT2,JT3,JT4,JT5,JT6,JT7;//declare text field JT1 to JT7 for user to type the corresponding answer 
+    JButton JB1,JB2;//declare two buttons JB1 and JB2 for user to click
+    
     AddNewCustomer(){
-        setLocation(600,200);
-        setSize(800,500);
+        setLocation(600,200);//set a location for this AddNewCustomer windows
+        setSize(800,500);// set size for the windows
         
+        //Declare and set the design of panel JP1
         JPanel JP1 = new JPanel();
         JP1.setLayout(null);
-        JP1.setBackground(new Color(173,216,230));
+        JP1.setBackground(new Color(173,216,230));//background with light blue
         
+        //initialize label and set Adding New Customer as title  
         JLabel label = new JLabel("Adding New Customer");
         label.setBounds(100, 10, 350, 35);
         label.setFont(new Font("Helvetica", Font.BOLD, 30));
         label.setForeground(new Color(0,0,139));
         JP1.add(label);
         
+        //initialize JL1 as customer name so the user will know what they should type 
         JL1 = new JLabel("Customer Name:");
         JL1.setBounds(100, 80, 100, 20);
         JL1.setFont(new Font("Times New Roman", Font.PLAIN,15));        
-        JT1 = new JTextField();
+        JT1 = new JTextField();//declare JT1 for input of customer name
         JT1.setBounds(240, 80, 200, 20);
         JP1.add(JL1);
         JP1.add(JT1);
         
+        //initialize JL2 as meter number
         JL2 = new JLabel("Meter Number:");
         JL2.setBounds(100, 120, 100, 20);
         JL2.setFont(new Font("Times New Roman", Font.PLAIN,15));        
@@ -44,73 +47,83 @@ public class AddNewCustomer extends JFrame implements ActionListener{
         JP1.add(JL2);
         JP1.add(JL11);
         
+        //initialize JL3 as address of user
         JL3 = new JLabel("Residental Address: ");
         JL3.setBounds(100, 160, 125, 20);
         JL3.setFont(new Font("Times New Roman", Font.PLAIN,15));        
-        JT3 = new JTextField();
+        JT3 = new JTextField();//declare JT3 for input of customer's address
         JT3.setBounds(240, 160, 200, 20);
         JP1.add(JL3);
         JP1.add(JT3);
         
+        //initialize JL5 as city of the customer
         JL5 = new JLabel("Residenal City: ");
         JL5.setBounds(100, 200, 110, 20);
         JL5.setFont(new Font("Times New Roman", Font.PLAIN,15));
-        JT5 = new JTextField();
+        JT5 = new JTextField();//declare JT5 for input of customer's address
         JT5.setBounds(240, 200, 200, 20);
         JP1.add(JL5);
         JP1.add(JT5);
         
+        //initialize JL4 as state of the customer
         JL4 = new JLabel("Residental State: ");
         JL4.setBounds(100, 240, 110, 20);
         JL4.setFont(new Font("Times New Roman", Font.PLAIN,15));
-        JT4 = new JTextField();
+        JT4 = new JTextField();//declare JT4 for input of customer's state
         JT4.setBounds(240, 240, 200, 20);
         JP1.add(JL4);
         JP1.add(JT4);
         
+        //initialize JL6 as email of the customer
         JL6 = new JLabel("Customer Email: ");
         JL6.setBounds(100, 280, 110, 20);
         JL6.setFont(new Font("Times New Roman", Font.PLAIN,15));
-        JT6 = new JTextField();
+        JT6 = new JTextField();//declare JT6 for input of customer's email
         JT6.setBounds(240, 280, 200, 20);
         JP1.add(JL6);
         JP1.add(JT6);
-        
+
+        //initialize JL7 as phone number of the customer
         JL7 = new JLabel("Customer Phone: ");
         JL7.setBounds(100, 320, 110, 20);
         JL7.setFont(new Font("Times New Roman", Font.PLAIN,15));
-        JT7 = new JTextField();
+        JT7 = new JTextField();//declare JT7 for input of customer's phone number
         JT7.setBounds(240, 320, 200, 20);
         JP1.add(JL7);
         JP1.add(JT7);
         
+        //initialize button JB1 as proceed,when user click this button, a new customer will be added
         JB1 = new JButton("Proceed");
-        JB1.setBounds(120, 390, 100, 25);        
+        JB1.setBounds(120, 390, 100, 25); 
+
+        //initialize button JB2 as cancel,when user click this button, the operation before will be canceled and     //there is no new customer be added
         JB2 = new JButton("Cancel");
         JB2.setBounds(250, 390, 100, 25);        
         
+        //set background design of proceed button as dark blue
         JB1.setBackground(new Color(0,0,139));
+        //set the font color of proceed button as white color
         JB1.setForeground(Color.WHITE);
         JB1.setFont(new Font("Times New Roman", Font.PLAIN,15));
         
+        //set background design of cancel button as white color
         JB2.setBackground(Color.WHITE);
+        //set the font color of cancel button as black color
         JB2.setForeground(Color.BLACK);
         JB2.setFont(new Font("Times New Roman", Font.PLAIN,15));
         
         JP1.add(JB1);
         JP1.add(JB2);
         setLayout(new BorderLayout());
-        
         add(JP1,"Center");
         
+        //insert an image to ease the customer understand what the windows function
+        //this windows functions as add new customer
         ImageIcon IC1 = new ImageIcon(ClassLoader.getSystemResource("icon/hicon1.png"));
         Image img3 = IC1.getImage().getScaledInstance(300, 300,Image.SCALE_DEFAULT);
         ImageIcon IC2 = new ImageIcon(img3);
         JL8 = new JLabel(IC2);
-        
-        
         add(JL8,"West");
-        //for changing the color of the whole Frame
         getContentPane().setBackground(new Color(255,253,208));
         
         JB1.addActionListener(this);
@@ -122,7 +135,9 @@ public class AddNewCustomer extends JFrame implements ActionListener{
         
     }
     public void actionPerformed(ActionEvent ae){
+        //if proceed button is pressed, the information of customer will be stored and a new customer is added
         if(ae.getSource() == JB1){
+
             String customer_name = JT1.getText();
             String customer_meter = JL11.getText();
             String customer_address = JT3.getText();
@@ -130,14 +145,15 @@ public class AddNewCustomer extends JFrame implements ActionListener{
             String customer_city = JT5.getText();
             String customer_email = JT6.getText();
             String customer_phone = JT7.getText();
-
+   
+            //the information will be insert to the table login and table customer in database
             String str1 = "insert into customer values('"+customer_name+"','"+customer_meter+"','"+customer_address+"','"+customer_city+"','"+customer_state+"','"+customer_email+"','"+customer_phone+"')";
             String str2 = "insert into login values('"+customer_meter+"', '', '', '', '')";
             try{
                 DBConnection con1 = new DBConnection();
                 con1.s.executeUpdate(str1);
                 con1.s.executeUpdate(str2);
-                JOptionPane.showMessageDialog(null," New Customer Details Successfully Added");
+                JOptionPane.showMessageDialog(null," New Customer Details Successfully Added");//dialog will be displayed if customer is added successfully 
                 this.setVisible(false);
                 new MeterDetails(customer_meter).setVisible(true);
 
