4e0a86d4da5f18379c674299d4401f2a32220c4d
USM-Cat201 <98682288+USM-Cat201@users.noreply.github.com>
2022-01-30 15:23:44 +0800

    Update BillsInformation.java

--- src/electricity/billing/system/BillsInformation.java
+++ src/electricity/billing/system/BillsInformation.java
@@ -1,29 +1,29 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
+/*this is the class for displaying bill information in a table form*/
 package electricity.billing.system;
 
-import java.awt.*;
-import java.awt.event.*;
 import javax.swing.*;
 import java.sql.*;
 import net.proteanit.sql.DbUtils;
+import java.awt.*;
+import java.awt.event.*;
 
 public class BillsInformation extends JFrame{
  
-    JTable JT1;
+    JTable JT1;//declare table JT1
+    //declare and initialize string meter number, month, units used, total amount of bills and payment status
     String x[] = {"Meter No","Month","Units used per month ","Total Amount of bills "," Payment Status"};
     String y[][] = new String[40][8];
     int i=0, j=0;
     BillsInformation(String meter){
-        super("Bill Information");
-        setSize(800,650);
+        super("Bills");
+        //set the size of panel
+        setSize(900,650);
         setLocation(500,250);
         setLayout(null);
-        getContentPane().setBackground(Color.LIGHT_GRAY);
+        getContentPane().setBackground(Color.LIGHT_GRAY);//set the background of panel as light gray
         
-        JT1 = new JTable(y,x);
+        JT1 = new JTable(y,x);//initialize JT1
         
         try{
             DBConnection c  = new DBConnection();
@@ -38,7 +38,7 @@ public class BillsInformation extends JFrame{
         
         
         JScrollPane span = new JScrollPane(JT1);
-        span.setBounds(50, 50, 550, 450);
+        span.setBounds(50, 50, 700, 450);
         add(span);
         
     }
