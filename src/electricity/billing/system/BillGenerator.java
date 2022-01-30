218a6280b7eae8d39e144897913e08c597d14c55
USM-Cat201 <98682288+USM-Cat201@users.noreply.github.com>
2022-01-30 15:19:34 +0800

    Update BillGenerator.java

--- src/electricity/billing/system/BillGenerator.java
+++ src/electricity/billing/system/BillGenerator.java
@@ -1,21 +1,20 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+/*class for generate bill function in customer homepage
+the bill amount will be retrieved form the database and display when bill is generated*/
 package electricity.billing.system;
 
-import java.awt.*;
-import java.awt.event.*;
 import javax.swing.*;
 import java.sql.*;
+import java.awt.*;
+import java.awt.event.*;
 
 public class BillGenerator extends JFrame implements ActionListener{
-    JButton JB1;
-    JPanel JP1;
-    JLabel JL1, JL2;
-    JTextArea JTA1;
-    Choice C1;
-    String meter;
+    JButton JB1;//declare button JB1
+    JPanel JP1;//declare panel JP1
+    JLabel JL1, JL2; //declare two labels which is JL1 and JL2
+    JTextArea JTA1; //declare textarea JTA1
+    Choice C1; //declare choice option C1
+    String meter; //declare string meter
+    
     BillGenerator(String meter){
         this.meter = meter;
         setLayout(new BorderLayout());
@@ -26,8 +25,8 @@ public class BillGenerator extends JFrame implements ActionListener{
         JL1 = new JLabel("Generate Bill");
         
         JL2 = new JLabel(meter);
+        //initialize the choice from January to December
         C1 = new Choice();
-        
         C1.add("January");
         C1.add("February");
         C1.add("March");
@@ -41,21 +40,22 @@ public class BillGenerator extends JFrame implements ActionListener{
         C1.add("November");
         C1.add("December");
 
-        
+        //leave the text so user know that they need to press the button to generate bills
         JTA1 = new JTextArea(50,15);
         JTA1.setText("\tPress 'Generate Bill' button");
         JScrollPane jsp = new JScrollPane(JTA1);
         JTA1.setFont(new Font("Helvetica",Font.BOLD,20));
-        
+
+        //initialize the button so user can generate bill after pressing this button
         JB1 = new JButton("Generate Bills");
         
         JP1.add(JL1);
         JP1.add(JL2);
         JP1.add(C1);
-        add(JP1,"North");
+        add(JP1,"North");//the text of generate bill and the month option will be shown at the top of the windows
         
         add(jsp,"Center");
-        add(JB1,"South");
+        add(JB1,"South");// the button will be shown at the bottom of windows
         
         JB1.addActionListener(this);
         
@@ -66,8 +66,9 @@ public class BillGenerator extends JFrame implements ActionListener{
             DBConnection c = new DBConnection();
    
             String month = C1.getSelectedItem();
-            JTA1.setText("TENAGA NASIONAL BERHAD\n ELECTRIC BILL OF "+month+",2022\n\n\n");
+            JTA1.setText("ELECTRICITY BILL OF "+month+",2022\n\n\n");
             
+            /*select customer details from customer table in database*/
             ResultSet rs = c.s.executeQuery("select * from customer where customer_meter="+meter);
             
             if(rs.next()){
@@ -81,7 +82,7 @@ public class BillGenerator extends JFrame implements ActionListener{
                 JTA1.append("\n**************************************************");
                 JTA1.append("\n");
             }
-            
+            /*select meter details from meter_details table in database*/
             rs = c.s.executeQuery("select * from meter_details where meter_number = " + meter);
             
             if(rs.next()){
@@ -91,32 +92,30 @@ public class BillGenerator extends JFrame implements ActionListener{
                 JTA1.append("\n    Billing Days:         "+rs.getString("billing_day"));
                 JTA1.append("\n");
             }
+            /*select the amount of taxes from taxes table in database*/
             rs = c.s.executeQuery("select * from taxes");
             if(rs.next()){
                 JTA1.append("---------------------------------------------------------------");
                 JTA1.append("\n\n");
                 JTA1.append("\n Cost per Unit:             RM "+rs.getString("cost_per_unit"));
                 JTA1.append("\n Meter Rent Fee:            RM "+rs.getString("rental_meter"));
-                JTA1.append("\n Service Taxes:             RM " +rs.getString("service_tax"));
-                JTA1.append("\n KWTBB                      RM "+rs.getString("KWTBB"));
+                JTA1.append("\n Service Taxes:              " +rs.getString("service_tax")+"%");
+                JTA1.append("\n KWTBB                       "+rs.getString("KWTBB")+"%");
                 JTA1.append("\n");
                 
             }
-            
+            /*select electricity bill details from electricBill table in database*/
             rs = c.s.executeQuery("select * from electricBill where meter="+meter+" AND bill_month = '"+C1.getSelectedItem()+"'");
             
             if(rs.next()){
-                JTA1.append("\n    Billing Month :     RM\t"+rs.getString("bill_month"));
-                JTA1.append("\n    Units Consumed:     RM \t"+rs.getString("units_used"));
+                JTA1.append("\n    Billing Month :     \t"+rs.getString("bill_month"));
+                JTA1.append("\n    Units Consumed:      \t"+rs.getString("units_used"));
                 JTA1.append("\n    Total Charges :     RM \t"+rs.getString("bill_amount"));
                 JTA1.append("\n---------------------------------------------------------------");
                 JTA1.append("\n    TOTAL AMOUNT PAID : RM  "+rs.getString("bill_amount"));
             }
             
-            
-            
-            
-            
+  
             
         }catch(Exception e){
             e.printStackTrace();
